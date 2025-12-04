1dd9b29d490cd8e2943e4159603a590dcb95cd37
YD.D.小钱钱 <winds8@gmail.com>
2025-12-04 16:17:35 +0800

    Normalize GDScript indentation

--- scripts/core/logger.gd
+++ scripts/core/logger.gd
@@ -4,31 +4,31 @@ enum LogLevel { DEBUG, INFO, WARNING, ERROR }
 static var _level: LogLevel = LogLevel.DEBUG
 
 static func set_level(level: LogLevel) -> void:
-	_level = level
+    _level = level
 
 static func debug(message: String) -> void:
-	_emit(LogLevel.DEBUG, message)
+    _emit(LogLevel.DEBUG, message)
 
 static func info(message: String) -> void:
-	_emit(LogLevel.INFO, message)
+    _emit(LogLevel.INFO, message)
 
 static func warn(message: String) -> void:
-	_emit(LogLevel.WARNING, message)
+    _emit(LogLevel.WARNING, message)
 
 static func error(message: String) -> void:
-	_emit(LogLevel.ERROR, message)
+    _emit(LogLevel.ERROR, message)
 
 static func _emit(level: LogLevel, message: String) -> void:
-	if level < _level:
-		return
-	var prefix: String = "[LOG]"
-	match level:
-		LogLevel.DEBUG:
-			prefix = "[DEBUG]"
-		LogLevel.INFO:
-			prefix = "[INFO]"
-		LogLevel.WARNING:
-			prefix = "[WARN]"
-		LogLevel.ERROR:
-			prefix = "[ERROR]"
-	print("%s %s" % [prefix, message])
+    if level < _level:
+        return
+    var prefix: String = "[LOG]"
+    match level:
+        LogLevel.DEBUG:
+            prefix = "[DEBUG]"
+        LogLevel.INFO:
+            prefix = "[INFO]"
+        LogLevel.WARNING:
+            prefix = "[WARN]"
+        LogLevel.ERROR:
+            prefix = "[ERROR]"
+    print("%s %s" % [prefix, message])
